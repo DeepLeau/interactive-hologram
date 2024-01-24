1f503dda5e6bc08fdf721349c3fec1c268ac0645
DeepLeau <120391346+DeepLeau@users.noreply.github.com>
2024-01-24 17:08:53 +0100

    Update audio.py

--- audio.py
+++ audio.py
@@ -9,7 +9,7 @@ p = pyaudio.PyAudio()
 WIDTH = 2
 RATE = int(p.get_default_input_device_info()['defaultSampleRate'])
 DEVICE = p.get_default_input_device_info()['index']
-alpha = 0.5  # Facteur de lissage (entre 0 et 1)
+alpha = 0.5  
 rms = 1
 
 def callback(in_data, frame_count, time_info, status):
@@ -20,7 +20,7 @@ def callback(in_data, frame_count, time_info, status):
     
     db = 20 * log10(rms)
 
-# A définir !!
+# Les variables seuil, x et y sont à changer selon tes besoins, x et y = coordonnées de la position où tu veux mettre la souris et seuil c'est le seuil à partir du quel tu considères que ca fasse un claquement de doigts
 
     seuil = 70
     x,y = 50,50
@@ -45,10 +45,9 @@ stream.start_stream()
 
 try:
     while stream.is_active():
-        time.sleep(0.1)  # Vous pouvez ajuster l'intervalle de temps si nécessaire
+        time.sleep(0.1)  
 
 except KeyboardInterrupt:
-    # Arrêtez le flux lorsqu'une interruption clavier se produit
     stream.stop_stream()
     stream.close()
     p.terminate()
