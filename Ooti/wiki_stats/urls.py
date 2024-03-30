119f9605c55e583d34dd79b2a0c3049682f48383
DeepLeau <120391346+DeepLeau@users.noreply.github.com>
2024-03-30 16:51:26 +0100

    Delete Ooti directory

--- Ooti/wiki_stats/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_wikipedia_summary,test_email
-
-urlpatterns = [
-    path('summary/', get_wikipedia_summary, name='summary'),
-    path('email/', test_email, name='email'),
-]
